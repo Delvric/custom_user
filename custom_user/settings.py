a2a198e57668551c75317a79ac1dee0531e690c8
Delvric <mr.tezeno@gmail.com>
2020-09-02 23:48:15 -0400

    custom 2

--- custom_user/settings.py
+++ custom_user/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'custom_user.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR)],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
